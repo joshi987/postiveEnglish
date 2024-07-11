49d7c0ac2006ed35c4d0fce93c06948cd21f263d
Mukesh Chandra <mukeshjoshi0301@gmail.com>
2024-07-11 11:51:28 +0530

    npm build install

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,21 @@
-const dotenv = require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const Registration = require('./models/Data');
-const userRoute = require("./routes/userRoute")
-const errorHandler = require('./controllers/errroMiddleware')
+ require('dotenv').config();
+import express, { json, urlencoded } from "express";
+import { connect } from "mongoose";
+import { json as _json } from "body-parser";
+import cors from "cors";
+import Registration from './models/Data';
+import userRoute from "./routes/userRoute";
+import errorHandler from './controllers/errroMiddleware';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
+app.use(_json());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:false})); 
+app.use(json());
+app.use(urlencoded({extended:false})); 
 
 //Routes
 app.use("/api/users", userRoute);
@@ -23,7 +23,7 @@ app.use("/api/users", userRoute);
 app.use(errorHandler);
 
 // Connect to DB and start server
-mongoose.connect(process.env.MONGO_URI,)
+connect(process.env.MONGO_URI,)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server Running on Port ${PORT}`);
