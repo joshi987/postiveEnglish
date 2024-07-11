a04bfbed4743635ba021d3bb03d22fea395a7890
Mukesh Chandra <mukeshjoshi0301@gmail.com>
2024-07-11 17:30:41 +0530

    updation in eslint-plugin-react@7.34.3

--- Frontend/src/componets/Footer.jsx
+++ Frontend/src/componets/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="bg-black text-white p-4 text-center">
-        Copyright © 2024 MindScape. All rights reserved.
+        Copyright © 2024 Brain Voyage. All rights reserved.
     </div>
   );
 }
