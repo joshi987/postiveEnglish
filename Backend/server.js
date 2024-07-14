574d810252613d8e53462bca61b739b41affaaa4
Mukesh Chandra <mukeshjoshi0301@gmail.com>
2024-07-14 15:25:36 +0530

    cors update

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,41 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// require('dotenv').config();
+// const cors = require("cors");
+// const Registration = require('./models/Data');
+// const userRoute = require("./routes/userRoute")
+// const errorHandler = require('./controllers/errroMiddleware')
+
+// const app = express();
+
+// const PORT = process.env.PORT || 5000;
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended:false})); 
+
+// //Routes
+// app.use("/api/users", userRoute);
+
+// app.use(errorHandler);
+
+// // Connect to DB and start server
+// mongoose.connect(process.env.MONGO_URI,)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server Running on Port ${PORT}`);
+//     });
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch(err => {
+//     console.error('Failed to connect to MongoDB', err);
+//   });
+
+// // Define a simple route
+
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -13,18 +51,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:false})); 
+const corsOptions = {
+  origin: 'https://your-frontend-domain.vercel.app', // Replace with your Vercel frontend domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
 
-//Routes
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use("/api/users", userRoute);
 
 app.use(errorHandler);
 
 // Connect to DB and start server
-mongoose.connect(process.env.MONGO_URI,)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server Running on Port ${PORT}`);
@@ -34,5 +79,3 @@ mongoose.connect(process.env.MONGO_URI,)
   .catch(err => {
     console.error('Failed to connect to MongoDB', err);
   });
-
-// Define a simple route
